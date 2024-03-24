2f473444c01af74990cc1b2e2a547ccacee22d0a
Avijit2002 <avijitram2013@gmail.com>
2024-03-25 02:06:14 +0530

    env added to sst config file

--- sst.config.ts
+++ sst.config.ts
@@ -12,7 +12,12 @@ export default {
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         environment: {
-          
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+          DATABASE_URL: process.env.DATABASE_URL!,
+          DIRECT_URL: process.env.DIRECT_URL!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID!,
+          DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET!,
         }
       });
 
